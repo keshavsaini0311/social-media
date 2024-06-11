60713889a98a2577fc75920e0c6a982c0991b61d
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-06-11 15:25:00 +0530

    added message.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
 import SignUp from "./pages/SignUp";
+import Message from "./pages/Message";
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp/>}/>
         <Route path='/' element={<SignIn/>}/>
         <Route element={<PrivateRoute />}>
-          
+          <Route path='/messages' element={<Message/>}/>
           <Route path='/home' element={<Home/>}/>
         </Route> 
       </Routes>
--- frontend/src/pages/Message.js
+++ frontend/src/pages/Message.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 export default function Message() {
   return (
-    <div>
-      Message
+    <div className="flex flex-grow">
+            <Sidebar/>
+            Message
     </div>
   )
 }
