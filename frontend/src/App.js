b26aae655d6f37e37140ebb2a938b282d7bf2929
nishant490713 <nishantkatkade49@gmail.com>
2024-06-13 19:31:35 +0530

    Socket io integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
 import SignUp from "./pages/SignUp";
+import { Messaging } from "./pages/Messaging";
 
 export default function App() {
   return (
@@ -10,8 +11,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path='/' element={<SignIn/>}/>
+        <Route path='/' element={<Messaging/>}/>
         <Route element={<PrivateRoute />}>
+
+        
           
           <Route path='/home' element={<Home/>}/>
         </Route> 
