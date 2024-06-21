1db4fe6fba6a763fac0dc406f3e80e64e1a95279
nishant490713 <nishantkatkade49@gmail.com>
2024-06-21 19:46:43 +0530

    middleware update

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -2,7 +2,8 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 const protectRoute = async (req, res, next) => {
-	const token=req.cookies.token
+	const token= req.headers['authorization'].split('Bearer ')[1]|| req?.body?.token||req?.cookie;
+      console.log('cookies from middleware ',req);
     if(!token){
         return res.status(401).json({ message: "Unauthorized" });
     }
